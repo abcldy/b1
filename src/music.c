851290f624e198d2cc59e0d86b6607e6b0fa47be
lyl <12345678@qq.com>
2024-09-13 02:11:10 +0800

    有bug，待修正的music

--- src/music.c
+++ src/music.c
@@ -1,4 +1,19 @@
-#include"main.h"
+#include "main.h"
+
+
+int x5,y5;
+void xy()//消抖部分
+{
+    int flag = 0;//获得的坐标的数量
+    while(1)
+    {
+        read(touch_fd , &touch , sizeof(touch));
+        if (touch.type == EV_ABS && touch.code == ABS_X && flag == 0) {x5 = touch.value*800/1024;flag = 1;}
+        if (touch.type == EV_ABS && touch.code == ABS_Y && flag == 1) {y5 = touch.value*480/600;flag = 2;}
+        if(flag == 2){flag = 0;break;}
+
+    }
+}
 
 
 /*
@@ -24,7 +39,7 @@ int send_cmd_music(  int fd_fifo , const char * cmd )
 }
 
 
-int music1()
+int music()
 {
     int  ret_val ;
     // 创建用于发送控制指令的管道文件
@@ -41,7 +56,7 @@ int music1()
 
     // 直接使用 popen 来播放音视频
     FILE * fp = popen("/dev/mplayer music.mp3 -quiet -slave -input file=/tmp/myFifo &", "r");
-
+    //FILE * fp = popen("/usr/bin/madplay music.mp3 -quiet -slave -input file=/tmp/myFifo &", "r");
     /*
         /dev/mplayer  3.avi  -geometry 100:200 -zoom -x 400 -y 200 &
         -geometry 100:200   设置视频播放的位置 
@@ -51,18 +66,18 @@ int music1()
     if ( fp == NULL )
     {
         perror("popen error");
-        return -1 ;
+        return -1;
     }
     
 
-    printf("音频正在播放....\n");
+    // printf("音频正在播放....\n");
 
     // 打开用于发送指令的管道文件
     int fd_fifo = open(FIFO_PATH , O_RDWR );
     if (-1 == fd_fifo)
     {
         perror("open fifo error");
-        return -1 ;
+        return -1;
     }
     
 
@@ -70,44 +85,124 @@ int music1()
         1.播放2.暂停/继续3.下一个视频4.结束5.快进5s
         6.退回5s
     */
-    int i=1;//输入
-    int n = 0;//播放/暂停
+    int i=0;//输入
+    //int n = 0;//播放/暂停
     int m = 0;//视频选择
+    int exit_f = 1;//判断是否退出,0则退出
     show_1152000bmp("music2.bmp", FB);
-    while(1)//开始输入命令
+    while(exit_f)//开始输入命令
     {
-        printf("1.播放 2.暂停/继续 3.下一个视频 4.退出 5.快进5s 6.退回5s \n");
+        // printf("1.播放 2.暂停/继续 3.下一个视频 4.退出 5.快进5s 6.退回5s \n");
+        
+        
+        // project_touch();
+        // if (touch.type == EV_KEY && touch.code == BTN_TOUCH && touch.value == 0)
+        // {
+
+        xy();
+            
+        if(y5>360&y5<400)
+        {
+            if(x5>250&x5<285)
+            {i=6;}
+            if(x5>320&x5<355)
+            {i=1;}
+            if(x5>380&x5<415)
+            {i=2;}
+            if(x5>450&x5<485)
+            {i=5;}
+            if(x5>770&y5<50)
+            {
+                i=4;
+            }
+
+        }
+        // if(x5>770&y5<50)
+        // {
+        // system("killall -9 mplayer");
+        //     interface();
+        //     break;
+        // }
+        //xy();
+        printf("%d,%d\n",x,y);
+            
+            
+            // xy();
+            // printf("%d,%d\n",x5,y5);
+            // // if(touch.type == EV_KEY && touch.code == BTN_TOUCH && touch.value == 0)
+            // // {
+            // if(x > 643 && x < 662 && y > 201 && y < 220)
+            // {
+            //     send_cmd_music(fd_fifo,"seek -5\n");//后退5秒
+            // }
+            // // if(x > 97 && x < 112 && y > 284 && y < 298)
+            // // {
+            
+            // // }
+            // if(x > 745 && x < 764 && y > 201 && y < 220)
+            // {
+            //     send_cmd_music(fd_fifo,"seek +5\n");//前进5秒
+            // }
+            // if(x > 688 && x < 719 && y > 169 && y < 226)
+            // {
+            //     send_cmd_music(fd_fifo,"pause\n");//暂停
+            // }
+
+            // if(x > 644 && x < 678 && y > 380 && y < 410)
+            // {
+            //     send_cmd_music(fd_fifo,"volume -10\n");//音量-10
+            // }
+            // if(x > 740 && x <772 && y > 348 && y < 410)
+            // {
+            //     send_cmd_music(fd_fifo,"volume +10\n");//音量+10
+            // }
+            // if(x > 770 && x < 800 && y > 0 && y < 30)
+            // {
+            //     system("killall -9 mplayer");
+            //     interface();
+            //     break;
+            // }
     
-    project_touch();
- 
-    if(x<400)
-    {
-    
-        if(y<240)
-        {
-            i=2;//暂停
-        }
-        else
-        {
-            i=1;//播放
-        }
+           
 
-    }
-    if(x>400)
-    {
-        if(y<240)
-        {
-            i=5;//快进
-        }
-        else
-        {
-            i=4;//退出
-        }
-    }
+        // if (touch.type == EV_KEY && touch.code == BTN_TOUCH && touch.value == 0)
+        // {   
+            
+        //     if(y>360&y<400)
+        //     {
+        //         if(x>250&x<285)
+        //         {
+                
+        //             send_cmd_music( fd_fifo , "seek -10\n" );//退10秒
+                    
+        //         }
+        //         if(x>320&x<355)
+        //         {
+        //             fp = popen("/dev/mplayer music.mp3 -quiet -slave -input file=/tmp/myFifo &", "r");//播放
+        //         }
+        //        if(x>380&x<415)
+        //         {
+        //          send_cmd_music( fd_fifo , "pause\n" );//暂停   
+        //         }
 
+        //         if(x>450&x<485)
+        //         {
+        //           send_cmd_music( fd_fifo , "seek +10\n" );  //前进10秒
+        //         }
 
+        //     }
+        //     else
+        //     {
+        //       if(x>770&y<50)
+        //         {
+        //         system("killall -9 mplayer");
+        //             interface();
+        //             break;
+        //         }  
+
+        //     }
         switch (i)
-        {
+        {      
             case 1:
             fp = popen("/dev/mplayer music.mp3 -quiet -slave -input file=/tmp/myFifo &", "r");
             break;
@@ -124,33 +219,34 @@ int music1()
             else
             {
                 system("killall -9 /dev/mplayer");//结束播放器
-                fp = popen("/dev/mplayer music.mp3 -quiet -slave -input file=/tmp/myFifo &", "r");
+                fp = popen("/dev/mplayer music(1).mp3 -quiet -slave -input file=/tmp/myFifo &", "r");
                 m = 0;
             }
             break;
             case 4:
             send_cmd_music( fd_fifo , "quit\n" );
+            interface();
+            exit_f = 0;
             break;
             case 5:
-            send_cmd_music( fd_fifo , "seek +5\n" );
+            send_cmd_music( fd_fifo , "seek +10\n" );
+            break;
             case 6:
-            send_cmd_music( fd_fifo , "seek -5\n" );
+            send_cmd_music( fd_fifo , "seek -10\n" );
             break;
-            case 0:
-            continue;
-            break;
+            
             default:
             break;
+            i = 0;
         }
         
 
-        sleep(1);
-    
+        //     //sleep(1);
+        // }
 
-        pause();
-
-     return 0;
-    }
-
-return 0;
+    }//while
+    pause();
+    return 0;
 }
+
+
