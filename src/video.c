b3864c5d52fcf6d1e774b4e4b579ffb13eeff0c0
daring02 <11869414+l498943966@user.noreply.gitee.com>
2024-09-12 10:39:10 +0800

    111

--- src/video.c
+++ src/video.c
@@ -3,6 +3,32 @@
 #define FIFO_PATH "/tmp/myFifo"
 
 
+FILE * fp;
+//视频链表定义
+// struct list_node{
+// 	char* video;  //视频名字
+	
+// 	struct list_node *prev;
+// 	struct list_node *next;
+// };
+
+// struct list_node n1,n2;
+// n1.video = "Redial.mp4";n1.prev = &n2;n1.next = &n2;
+// n2.video = "sb.mp4";n2.prev = &n1;n2.next = &n1;
+
+// struct list_node *init_list_node()
+// {
+// 	struct list_node *head = NULL;
+// 	head = (struct list_node *)malloc(sizeof(struct list_node));
+// 	if(head == NULL)
+// 	printf("malloc head error!\n");	
+// 	head->prev = head;
+// 	head->next = head;
+	
+// 	return head;
+	
+// };
+
 int send_cmd(  int fd_fifo , const char * cmd )
 {
 
@@ -22,6 +48,7 @@ int send_cmd(  int fd_fifo , const char * cmd )
 int video(int argc, char const *argv[])
 {
     int  ret_val ;
+    
     // 创建用于发送控制指令的管道文件
     if (access( FIFO_PATH , F_OK )) // 判断管道文件是否已存在
     {
@@ -33,7 +60,10 @@ int video(int argc, char const *argv[])
         }
         printf("管道文件创建成功\n");
     }
+    
 
+	// sprintf(buf,"/dev/mplayer -quiet -slave -zoom -x 600 -y 480 -input file=/tmp/myFifo & '/run/%s'",p->video);//拼接字符串到buf里
+	// fp=popen(buf,"r"); 
     // 直接使用 popen 来播放音视频
     FILE * fp = popen("/dev/mplayer /run/Redial.mp4 -quiet -slave -geometry 0:0 -zoom -x 600 -y 480 -input file=/tmp/myFifo &", "r");
 
@@ -66,10 +96,11 @@ int video(int argc, char const *argv[])
     */
     int i=0;//输入
     
-    int m = 0;//视频选择
+    
     show_location_bmp("bofangqi.bmp",600,0,FB);
     while(1)//开始输入命令
-    {
+    {   
+        int m = 0;//视频选择
         int n = 0;//播放/暂停
         // printf("1.播放 2.暂停/继续 3.下一个视频 4.退出 5.快进5s 6.退回5s \n");
         //触摸屏函数
@@ -77,32 +108,51 @@ int video(int argc, char const *argv[])
         //压力值判断下
         if (touch.type == EV_KEY && touch.code == BTN_TOUCH && touch.value == 0)
         {
-            // if(x > 95 && x < 112 && y > 123 && y < 136)
-            // {
-            //     printf("up\n");//上
-            // }
+            if(x > 95 && x < 112 && y > 123 && y < 136)
+            {
+                if(m==0)
+                {
+                    m=1;
+                    system("killall -9 mplayer");
+                    
+                    fp = popen("/dev/mplayer /run/hml.mp4 -quiet -slave -geometry 0:0 -zoom -x 600 -y 480 -input file=/tmp/myFifo &", "r");//上
+                }
+                else
+                {
+                    m=0;
+                    system("killall -9 mplayer");
+                   
+                    fp = popen("/dev/mplayer /run/Redial.mp4 -quiet -slave -geometry 0:0 -zoom -x 600 -y 480 -input file=/tmp/myFifo &", "r");//上
+                }
+               
+            }
             if(x > 643 && x < 662 && y > 201 && y < 220)
             {
                 send_cmd(fd_fifo,"seek -5\n");//后退5秒
             }
-            // if(x > 97 && x < 112 && y > 284 && y < 298)
-            // {
-            //     printf("down\n");//下
-            // }
+            if(x > 97 && x < 112 && y > 284 && y < 298)
+            {
+               if(m==0)
+                {
+                    m=1;
+                    system("killall -9 mplayer");
+                    
+                    fp = popen("/dev/mplayer /run/hml.mp4 -quiet -slave -geometry 0:0 -zoom -x 600 -y 480 -input file=/tmp/myFifo &", "r");//上
+                }
+                else
+                {
+                    m=0;
+                    system("killall -9 mplayer");
+                    
+                    fp = popen("/dev/mplayer /run/Redial.mp4 -quiet -slave -geometry 0:0 -zoom -x 600 -y 480 -input file=/tmp/myFifo &", "r");//上
+                }//下
+            }
             if(x > 745 && x < 764 && y > 201 && y < 220)
             {
                 send_cmd(fd_fifo,"seek +5\n");//前进5秒
             }
             if(x > 688 && x < 719 && y > 169 && y < 226)
             {
-                // if(n==1){
-                //     show_location_bmp("pause.bmp",600,0,FB);
-                //     n==0;
-                // }
-                // else
-                // {
-                //     n==1;
-                // }
                 send_cmd(fd_fifo,"pause\n");//暂停
             }
 
